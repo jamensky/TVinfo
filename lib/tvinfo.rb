b45c1db8c8ca6d11ca4b4065e962490ba6cf5e25
Jamal Rjoul <jamensky1991@gmail.com>
2020-03-05 19:59:21 +0000

    set css selector for retrieving show names

--- lib/tvinfo.rb
+++ lib/tvinfo.rb
@@ -4,6 +4,5 @@ require_relative "tvinfo/scraper"
 require_relative "tvinfo/cli"
 
 
-bat = Tv_show.new
 
-puts Tv_show.all[0].title
+Scraper.scrape
--- lib/tvinfo/scraper.rb
+++ lib/tvinfo/scraper.rb
@@ -5,12 +5,21 @@ require "pry"
 class Scraper
  attr_accessor :title, :ratings, :description, :cast
 
- #@@doc = Nokogiri::HTML(open("https://www.listal.com/list/television-giso6150"))
+ @@doc = Nokogiri::HTML(open("https://www.listal.com/list/television-giso6150")).css(".pure-u-1").css("a")
 
- def scrape
-    doc = Nokogiri::HTML(open("https://www.listal.com/list/television-giso6150"))
+ def self.scrape
+   @@doc.each do |show|
 
-    #binding.pry
+    new_show = Tv_show.new
+    new_show.title = show.text
+    binding.pry
+
+    new_show.ratings =
+    new_show
+    new_show
+
+
+   end
  end
 
 end
