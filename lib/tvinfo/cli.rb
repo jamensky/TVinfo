fa5f8aa26330aeb7ca78c9b1b079ee0356f9b651
Jamal Rjoul <jamensky1991@gmail.com>
2020-03-05 20:45:44 +0000

    started CLI class, created welcome text

---
+++ lib/tvinfo/cli.rb
@@ -0,0 +1,18 @@
+class Cli
+
+ def start
+   puts <<-welcome
+    Welcome to TV-info!
+
+    Here you can find information about the most successful TV-Shows ever created.
+
+    Let's start by taking your name...
+
+
+
+
+   welcome
+
+ end
+
+end 
